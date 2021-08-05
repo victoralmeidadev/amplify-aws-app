6c6878ff466103a68dc55abe913a0b094a1ccb28
Victor Almeida <victooralmeida1@gmail.com>
2021-08-05 17:36:55 -0300

    feat: deeplinking test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <h1>Hello from V2</h1>
+
+                <a href='exp://192.168.1.134:19000/--/'>Deeplinking test</a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
